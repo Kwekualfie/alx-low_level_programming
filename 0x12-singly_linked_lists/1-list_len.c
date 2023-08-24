8e98cf5ec59101e6fac00df013059a97277f1428
Alfred Dadzie Essiedu <kwekuessiedu143@gmail.com>
2023-08-24 16:54:07 -0700

    102

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include "lists.h"
-/**
 
- *list_len - Calculates the number of elements in a linked list
- *@h: Pointer to the list_t list to be evaluated
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- *Return: Total count of elements in h
-*/
+ * Return: number of elements in h
+ */
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,13 +1,14 @@
-#include <stdlib.h>
+i#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+
 /**
-* add_node_end - Appends a new node at the tail of a linked list
-* @head: Double pointer to the list_t list
-*
-* @str: String to be included in the new node
-* Return: Address of the newly appended element, or NULL if operation is unsuccessful 
-*/
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to put in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
